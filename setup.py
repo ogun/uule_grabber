36be0d53bcf67a07d0233ee840227c27a9da24e4
Kemal Ogün Işık <ogunisik@gmail.com>
2018-01-11 15:55:29 +0300

    uule grabber is created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="uule_grabber",
+    packages=["uule_grabber"],
+    version="0.1.3",
+    description="Generates UULE codes for Google Search ",
+    author="Kemal Ogun Isik",
+    author_email="ogunisik@gmail.com",
+    url="https://github.com/ogun/uule_grabber",
+    download_url="https://github.com/ogun/uule_grabber/archive/0.1.3.tar.gz",
+    classifiers=[],
+)
---
+++ uule_grabber/__init__.py
@@ -0,0 +1,1 @@
+from .uule_grabber import uule
