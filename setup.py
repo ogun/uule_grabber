221eea0124e3189d5886f5cd235c282f586ac965
Kemal Ogun Isik <ogun.isik@deliveryhero.com>
2023-02-05 12:08:31 +0100

    Setup workflows (#3)
    * Add new test case
    
    * Include README.md in PyPI
    
    * Rename the test step

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="uule_grabber",
     packages=["uule_grabber"],
-    version="0.1.5",
+    version="0.1.6",
     description="Generates UULE codes for Google Search",
-    long_description="Generates UULE codes for Google Search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Kemal Ogun Isik",
     author_email="ogunisik@gmail.com",
     url="https://github.com/ogun/uule_grabber",
-    download_url="https://github.com/ogun/uule_grabber/archive/v0.1.5.tar.gz",
+    download_url="https://github.com/ogun/uule_grabber/archive/v0.1.6.tar.gz",
     classifiers=[],
 )
