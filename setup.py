1ae6057adb7af997be66fe9efa954d1209022265
Kemal Ogun Isik <ogun.isik@deliveryhero.com>
2023-04-01 15:42:33 +0200

    Bump version to v0.1.9

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="uule_grabber",
     packages=["uule_grabber"],
-    version="0.1.8",
+    version="0.1.9",
     description="Generates UULE codes for Google Search",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Kemal Ogun Isik",
     author_email="ogunisik@gmail.com",
     url="https://github.com/ogun/uule_grabber",
-    download_url="https://github.com/ogun/uule_grabber/archive/v0.1.8.tar.gz",
+    download_url="https://github.com/ogun/uule_grabber/archive/v0.1.9.tar.gz",
     classifiers=[],
 )
